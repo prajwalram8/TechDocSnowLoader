e8bcb0d8923dd2a546585a3bb94218ee2fc7a266
Prajwal Gowda <Prajwal.G@gagroup.net>
2024-05-09 13:19:44 +0400

    Added Trigger.bat

---
+++ Trigger.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "c:\Users\Sachin.bm\Documents\Data Loaders\TechDocSnowLoader"
+"c:\Users\Sachin.bm\Documents\Data Loaders\TechDocSnowLoader\techdocEnv\Scripts\python.exe" -m src.main
